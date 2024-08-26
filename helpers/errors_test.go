a2940f6c70e9b11227c4d7d912de826e033341ea
Jeremy Lewi <jeremy@lewi.us>
2024-08-26 16:18:15 -0700

    Fix panic in ListOfErrors if final is null.
    * Allow for Final to not be null.
    
    * This was causing us to panic when trying to compute the error string.

--- helpers/errors_test.go
+++ helpers/errors_test.go
@@ -53,3 +53,43 @@ func Test_IsTypeError(t *testing.T) {
 		})
 	}
 }
+
+func Test_ListOfErrors(t *testing.T) {
+	type testCase struct {
+		name     string
+		input    ListOfErrors
+		expected string
+	}
+
+	cases := []testCase{
+		{
+			name: "basic",
+			input: ListOfErrors{
+				Final: errors.New("final"),
+				Causes: []error{
+					errors.New("cause 1"),
+					errors.New("cause 2"),
+				},
+			},
+			expected: "final; Causes: cause 1, cause 2",
+		},
+		{
+			name: "no-final",
+			input: ListOfErrors{
+				Causes: []error{
+					errors.New("cause 1"),
+					errors.New("cause 2"),
+				},
+			},
+			expected: "List of Errors: cause 1, cause 2",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.input.Error()
+			if got != tc.expected {
+				t.Errorf("expected %v; got %v", tc.expected, got)
+			}
+		})
+	}
+}
